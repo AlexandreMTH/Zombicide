3511bc86c4131ba2d2bd4c97360d562d9cc558b0
StellaLLage <stee_q@hotmail.com>
2018-06-18 20:58:30 -0300

    Turno na HUD

--- projeto zumbi 2/Assets/Scripts/Exit.cs
+++ projeto zumbi 2/Assets/Scripts/Exit.cs
@@ -1,24 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public GameObject fimFase;
 
 	void OnTriggerEnter(Collider col){
 
 		if (col.tag == "Player"){
 
-			//if (HUD.objetivoColetado >= 4)
-			player.Exp += 5;
-			HUD.objetivoColetado +=1;
-			print(player.Exp);
-			Destroy(this.gameObject, 0.1f);
+			if (HUD.objetivoColetado >= 4){ //falta saber se tem zumbi na área
+				StartCoroutine(WaitForTheEnd());				
+			}
 		}
+
+	}
+
+	IEnumerator WaitForTheEnd(){
+
+		yield return new WaitForSeconds(0.5f);
+		fimFase.SetActive(true);	
+
+	}
+
+	public void Menu(){
+		SceneManager.LoadScene("Menu");
+	}
+
+	public void Fase2(){
+
+		SceneManager.LoadScene("Fase C12");
 
 	}
 
