f3ba110913bab6aced5514172020cfea616c6630
StellaLLage <stee_q@hotmail.com>
2018-06-18 17:02:25 -0300

    Menu funcionando

---
+++ projeto zumbi 2/Assets/Scripts/Interface/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public GameObject fases, creditos;
+	static bool faseAtiva, creditosAtiva; 
+
+	// Use this for initialization
+
+	public void Jogar(){
+		if (!faseAtiva) fases.SetActive(!faseAtiva);
+		faseAtiva = !faseAtiva;	
+	}
+
+	public void Creditos(){
+		if (!creditosAtiva) creditos.SetActive(!creditosAtiva);	
+		else creditos.SetActive(!creditosAtiva);
+		creditosAtiva = !creditosAtiva;
+	}
+
+	public void Sair(){
+		Application.Quit();
+	}
+
+	public void Fase1(){
+		SceneManager.LoadScene("Fase A7");
+	}
+
+	public void Fase2(){
+		SceneManager.LoadScene("Fase C12");
+	}
+}
