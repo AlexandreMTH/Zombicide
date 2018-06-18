17edf15ee48a576fbedf23da46bbe33c53313d2c
StellaLLage <stee_q@hotmail.com>
2018-06-18 19:32:54 -0300

    Coisas da hUD atualizadas

---
+++ projeto zumbi 2/Assets/Scripts/Exit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+
+		if (col.tag == "Player"){
+
+			//if (HUD.objetivoColetado >= 4)
+			player.Exp += 5;
+			HUD.objetivoColetado +=1;
+			print(player.Exp);
+			Destroy(this.gameObject, 0.1f);
+		}
+
+	}
+
+}
