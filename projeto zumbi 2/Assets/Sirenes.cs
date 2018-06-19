715fa87312c534213fbbb4126c390ed2a1491871
frere <felipe.frere@hotmail.com>
2018-06-18 21:46:45 -0300

    comecei a adicionar a viatura

---
+++ projeto zumbi 2/Assets/Sirenes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sirenes : MonoBehaviour {
+
+	public GameObject LuzVer, LuzAzul;
+	bool controleLuzVer, controleLuzAzul;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		LuzVer.SetActive (controleLuzVer);
+		LuzAzul.SetActive (controleLuzAzul);
+
+		timer += Time.deltaTime;
+
+		if (timer > 0.3f && controleLuzAzul == true) {
+
+			timer = 0;
+			controleLuzAzul = false;
+			controleLuzVer = true;
+
+
+		}
+
+		if (timer > 0.3f && controleLuzVer == true) {
+
+			timer = 0;
+			controleLuzVer = false;
+			controleLuzAzul = true;
+
+
+		}
+
+
+	}
+}
